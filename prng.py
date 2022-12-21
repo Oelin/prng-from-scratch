94a5121286478e2cda914a3325e55405a0397dab
Oelin <42823429+Oelin@users.noreply.github.com>
2022-12-21 11:53:26 +0000

    Update prng.py

--- prng.py
+++ prng.py
@@ -5,15 +5,15 @@ MODULUS = int('1'*607, 2) # M607 - a large prime number.
 GENERATOR = 7
 
 
-def one_way_function(x): # The discrete logarithm problem.
+def one_way_function(x: int) -> int:
         return pow(GENERATOR, x, MODULUS)
 
 
-def hard_core_predicate(state, seed): # XOR-reduction on x * y.
-        bits = (int(bit) for bit in '{:b}'.format(x * y))
-
+def hard_core_predicate(state: int, seed: int) -> int:
+        
+        bits = (int(bit) for bit in '{:b}'.format(state * seed))
         return reduce(lambda x, y: x ^ y, bits)
-    
+
 
 def prng(seed: int, state: int) -> int:
 
